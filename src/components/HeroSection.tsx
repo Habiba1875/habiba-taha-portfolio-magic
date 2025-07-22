e3474b321719a60df55ca71ab7823b2a150d9804
Habiba1875 <het18750@gmail.com>
2025-07-22 07:20:42 +0300

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -70,11 +70,9 @@ const HeroSection = () => {
         </div>
         
         {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
 <a href="#about" className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
   <ArrowDown className="w-6 h-6 text-primary cursor-pointer" />
 </a>
-        </div>
       </div>
     </section>
   );
